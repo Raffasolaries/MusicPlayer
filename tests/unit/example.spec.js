21d1080833d7a964c0ecab23ed1fec43f83df82e
Raffaele Superga <rs@tantosvago.it>
2019-11-25 19:34:54 +0100

    project done

--- tests/unit/example.spec.js
+++ tests/unit/example.spec.js
@@ -2,7 +2,7 @@ import { shallowMount } from '@vue/test-utils'
 import VideosList from '@/components/VideosList.vue'
 
 describe('VideosList.vue', () => {
-  it('as the component starts, it will be loading', () => {
+  it('as the component starts, it will be loading', async () => {
     const wrapper = shallowMount(VideosList)
     expect(wrapper.vm.loading).toBe(true)
   })
